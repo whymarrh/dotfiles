1fc4fc23d5cb73d400f870d9a75d0b4bdd02639d
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-09-06 22:48:39 -0230

    Added install script.

---
+++ install.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -sO https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+curl -sO https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
+
+mv git-completion.bash .git-completion.bash
+mv git-prompt.sh .git-prompt.sh
+
+find . -name ".*" -not -name "." -not -name ".git" -exec cp -r '{}' $HOME/ \;
