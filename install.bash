f17201e2a3568af6cf91f91a608a5e19d1acf9c6
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-10-01 00:27:43 -0230

    Reorder install steps

--- install.bash
+++ install.bash
@@ -3,10 +3,10 @@
 rm -f .git-completion.bash
 rm -f .git-prompt.sh
 
-curl -sO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -sO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -sO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
-mv git-completion.bash .git-completion.bash
 mv git-prompt.sh .git-prompt.sh
+mv git-completion.bash .git-completion.bash
 
 find . -name ".*" -not -name "." -not -name ".git" -not -name ".sublime" -not -name ".osx" -exec cp -r '{}' "$HOME/" \;
